bfe10088a16ab8ea89e1696d1ebc1428a0ecda6b
artisyn <artioms1@gmail.com>
2022-02-07 01:43:05 +0200

    done login part

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 'use strict';
+const userName = document.querySelector('.userLogin__form--username');
+const userPassword = document.querySelector('.userLogin__form--password');
+const loginBtn = document.querySelector('.userLogin__form--btn');
+const loginWrapper = document.querySelector('.loginWrapper');
+const welcomeMessage = document.querySelector('.welcomeMessage');
+// const username = document.querySelector('');
+// const username = document.querySelector('');
+
+// functions
+
+const hideLogin = () => {
+  loginWrapper.classList.add('fade');
+  setTimeout(() => {
+    loginWrapper.classList.add('displayNone');
+    welcomeMessage.classList.remove('displayNone');
+    welcomeMessage.classList.add('fadeInOut');
+    setTimeout(() => {
+      welcomeMessage.classList.add('displayNone');
+    }, 6000);
+  }, 4000);
+};
+
+loginBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const pass = +userPassword.value;
+
+  if (pass === 3334) hideLogin();
+});
