1384d7b69fce01852d402375de24c8d56d71ff28
artisyn <artioms1@gmail.com>
2022-02-08 00:43:54 +0200

    almost done burger nav

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 'use strict';
-const userName = document.querySelector('.userLogin__form--username');
-const userPassword = document.querySelector('.userLogin__form--password');
-const loginBtn = document.querySelector('.userLogin__form--btn');
+const userName = document.querySelector('.login__username');
+const userPassword = document.querySelector('.login__password');
+const loginBtn = document.querySelector('.login__btn');
 const loginWrapper = document.querySelector('.loginWrapper');
-const welcomeMessage = document.querySelector('.welcomeMessage');
+const welcomeMessage = document.querySelector('.login__welcome-msg');
+const wrongPassMsg = document.querySelector('.login__wrong-pass');
 // const username = document.querySelector('');
 // const username = document.querySelector('');
 
 // functions
 
-const hideLogin = () => {
+const hideLogin = (user) => {
   loginWrapper.classList.add('fade');
   setTimeout(() => {
     loginWrapper.classList.add('displayNone');
@@ -17,13 +18,28 @@ const hideLogin = () => {
     welcomeMessage.classList.add('fadeInOut');
     setTimeout(() => {
       welcomeMessage.classList.add('displayNone');
+      welcomeMessage.classList.remove('fadeInOut');
+      /// user
     }, 6000);
   }, 4000);
 };
+const displayBadPass = () => {
+  wrongPassMsg.classList.remove('noOpacity');
+  setTimeout(() => {
+    wrongPassMsg.classList.add('noOpacity');
+  }, 2000);
+};
+
+// login logic
 
 loginBtn.addEventListener('click', (e) => {
   e.preventDefault();
   const pass = +userPassword.value;
+  const user = userName.value;
+  userPassword.value = '';
 
-  if (pass === 3334) hideLogin();
+  if (pass === 3334) hideLogin(user);
+  if (pass !== 3334) displayBadPass();
 });
+
+// header logic
